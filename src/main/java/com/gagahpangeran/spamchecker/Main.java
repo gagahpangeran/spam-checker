35a57d3159ba60b36fdd84b5a5c17d7abbff179f
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2023-04-03 14:41:01 +0700

    use spam checker in main method

--- src/main/java/com/gagahpangeran/spamchecker/Main.java
+++ src/main/java/com/gagahpangeran/spamchecker/Main.java
@@ -1,7 +1,17 @@
 package com.gagahpangeran.spamchecker;
 
+import com.gagahpangeran.spamchecker.checker.SpamChecker;
+
 public class Main {
+    private static String[] data = new String[] {
+            "This is a spam email",
+            "This is not a spam email",
+            "Hello how are you?",
+            "What a great day today",
+    };
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpamChecker checker = new SpamChecker(data);
+        checker.getVerdict();
     }
 }
