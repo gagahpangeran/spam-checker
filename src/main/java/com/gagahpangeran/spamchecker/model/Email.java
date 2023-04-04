494811c2492a56fb857889ca80a09efd28c6e0ba
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2023-04-04 10:10:50 +0700

    add similarity scores array in Email model

--- src/main/java/com/gagahpangeran/spamchecker/model/Email.java
+++ src/main/java/com/gagahpangeran/spamchecker/model/Email.java
@@ -1,14 +1,18 @@
 package com.gagahpangeran.spamchecker.model;
 
+import java.util.ArrayList;
+
 public class Email {
     private int id;
     private String content;
     private double spamProbability;
+    private ArrayList<Double> similarityScores;
 
     public Email(int id, String content) {
         this.id = id;
         this.content = content;
         this.spamProbability = 0;
+        this.similarityScores = new ArrayList<>();
     }
 
     public int getId() {
@@ -17,6 +21,10 @@ public class Email {
 
     public String getContent() {
         return this.content;
+    }
+
+    public void addSimilarityScore(double score) {
+        this.similarityScores.add(score);
     }
 
     public double getSpamProbability() {
