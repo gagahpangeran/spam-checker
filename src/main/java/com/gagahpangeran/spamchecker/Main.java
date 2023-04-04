b45c95a9b8a2241b8cd3271aa60cfb90ddf4e600
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2023-04-04 10:15:59 +0700

    use calculateSpamProbaility method in main class

--- src/main/java/com/gagahpangeran/spamchecker/Main.java
+++ src/main/java/com/gagahpangeran/spamchecker/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         SpamChecker checker = new SpamChecker(data);
+        checker.calculateSpamProbability();
         checker.getVerdict();
     }
 }
