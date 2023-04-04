3d22a6cdd2958d3190f3cbc8d895c4ac03587474
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2023-04-04 14:23:08 +0700

    change LCS implementation to compare word instead of character

--- src/main/java/com/gagahpangeran/spamchecker/comparator/LongestCommonSubsequenceComparator.java
+++ src/main/java/com/gagahpangeran/spamchecker/comparator/LongestCommonSubsequenceComparator.java
@@ -3,14 +3,22 @@ package com.gagahpangeran.spamchecker.comparator;
 public class LongestCommonSubsequenceComparator implements TextComparator {
     @Override
     public double getSimilarity(String text1, String text2) {
-        int t1Len = text1.length();
-        int t2Len = text2.length();
+        // convert to lowercase and remove punctuation
+        text1 = text1.toLowerCase().replaceAll("\\p{Punct}", "");
+        text2 = text2.toLowerCase().replaceAll("\\p{Punct}", "");
+
+        // split text by whitespace into array
+        String[] t1Arr = text1.split("\\s+");
+        String[] t2Arr = text2.split("\\s+");
+
+        int t1Len = t1Arr.length;
+        int t2Len = t2Arr.length;
 
         int[][] dp = new int[t1Len + 1][t2Len + 1];
 
         for (int i = 1; i <= t1Len; i++) {
             for (int j = 1; j <= t2Len; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                if (t1Arr[i - 1].equals(t2Arr[j - 1])) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
                     dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
