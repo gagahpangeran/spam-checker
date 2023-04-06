5955ee47404dcd8eba3155e6f9cadbdb113ef919
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2023-04-06 09:39:45 +0700

    add cosine similarity comparator

---
+++ src/main/java/com/gagahpangeran/spamchecker/comparator/CosineSimilarityComparator.java
@@ -0,0 +1,54 @@
+package com.gagahpangeran.spamchecker.comparator;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class CosineSimilarityComparator extends AbstractTextComparator {
+    @Override
+    public double getSimilarity(String text1, String text2) {
+        String[] t1Arr = this.processString(text1);
+        String[] t2Arr = this.processString(text2);
+
+        HashMap<String, Integer> t1Counter = getWordCount(t1Arr);
+        HashMap<String, Integer> t2Counter = getWordCount(t2Arr);
+
+        double dotProduct = calculateDotProduct(t1Counter, t2Counter);
+
+        double norm1 = 0;
+        for (Integer value : t1Counter.values()) {
+            norm1 += Math.pow(value, 2);
+        }
+
+        double norm2 = 0;
+        for (Integer value : t2Counter.values()) {
+            norm2 += Math.pow(value, 2);
+        }
+
+        double similarity = dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
+
+        return similarity;
+    }
+
+    private HashMap<String, Integer> getWordCount(String[] textArr) {
+        HashMap<String, Integer> wordCount = new HashMap<>();
+
+        for (String word : textArr) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        return wordCount;
+    }
+
+    private double calculateDotProduct(HashMap<String, Integer> vector1, HashMap<String, Integer> vector2) {
+        // get all the same words between two vector
+        HashSet<String> intersection = new HashSet<>(vector1.keySet());
+        intersection.retainAll(vector2.keySet());
+
+        double result = 0;
+        for (String word : intersection) {
+            result += vector1.get(word) * vector2.get(word);
+        }
+
+        return result;
+    }
+}
